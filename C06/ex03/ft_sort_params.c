ea78705dbd8cb64286d6e8192e534a2deea733db
Medbelh <162881636+Medbelh@users.noreply.github.com>
2024-07-11 14:39:48 +0100

    Add files via upload

---
+++ C06/ex03/ft_sort_params.c
@@ -0,0 +1,66 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort_params.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mbelhoua <mbelhoua@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/11 09:25:46 by mbelhoua          #+#    #+#             */
+/*   Updated: 2024/07/11 09:50:09 by mbelhoua         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+int	ft_strcmp(char *s1, char *s2)
+{
+	int	i;
+
+	i = 0;
+	while (s1[i] == s2[i] && (s1[i] != '\0' || s2[i] != '\0'))
+		i++;
+	return (s1[i] - s2[i]);
+}
+
+void	ft_sort_params(int ac, char **av)
+{
+	char	*p;
+	int		i;
+
+	i = 1;
+	while (i < ac - 1)
+	{
+		if (ft_strcmp(av[i], av[i + 1]) > 0)
+		{
+			p = av[i];
+			av[i] = av[i + 1];
+			av[i + 1] = p;
+			i = 0;
+		}
+		i++;
+	}
+}
+
+void	putstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		write(1, &str[i], 1);
+		i++;
+	}
+	write(1, "\n", 1);
+}
+
+int	main(int ac, char **av)
+{
+	int	i;
+
+	i = 1;
+	ft_sort_params(ac, av);
+	while (i < ac)
+		putstr(av[i++]);
+	return (0);
+}
