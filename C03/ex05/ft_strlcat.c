efbcb147c2c28f7405e56f936ed9c5cc21b53f0d
Medbelh <162881636+Medbelh@users.noreply.github.com>
2024-07-16 10:17:26 +0100

    Add files via upload

---
+++ C03/ex05/ft_strlcat.c
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mbelhoua <mbelhoua@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/16 09:30:21 by mbelhoua          #+#    #+#             */
+/*   Updated: 2024/07/16 09:45:33 by mbelhoua         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
+{
+	unsigned int	dest_len;
+	unsigned int	src_len;
+	unsigned int	i;
+	unsigned int	result;
+
+	i = 0;
+	dest_len = ft_strlen(dest);
+	src_len = ft_strlen(src);
+	if (dest_len < size)
+		result = dest_len + src_len;
+	else
+		result = src_len + size;
+	if (size == 0)
+		return (result);
+	while (src[i] && dest_len + i < size - 1)
+	{
+		dest[dest_len + i] = src[i];
+		i++;
+	}
+	dest[dest_len + i] = '\0';
+	return (result);
+}
