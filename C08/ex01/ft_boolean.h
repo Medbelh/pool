7dc34ceb33f21f5a0e8e9878f44a15ffa4d89c9c
Medbelh <162881636+Medbelh@users.noreply.github.com>
2024-07-18 17:04:15 +0100

    Add files via upload

---
+++ C08/ex01/ft_boolean.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_boolean.h                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mbelhoua <mbelhoua@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/18 14:53:39 by mbelhoua          #+#    #+#             */
+/*   Updated: 2024/07/18 14:53:49 by mbelhoua         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_BOOLEAN_H
+# define FT_BOOLEAN_H
+
+# include <unistd.h>
+
+# define FALSE 0
+# define TRUE 1
+# define EVEN(number) (number % 2 == 0)
+# define EVEN_MSG "I have an even number of arguments.\n"
+# define ODD_MSG "I have an odd number of arguments.\n"
+# define SUCCESS 0
+
+typedef int	t_bool;
+
+#endif
